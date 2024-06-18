291c182889b4595065631ae61349d39732092186
Houssam-re <rekibi.houssam@univ-ouargla.dz>
2024-06-18 01:28:28 +0100

    Add project files.

---
+++ Cons/Program.cs
@@ -0,0 +1,28 @@
+﻿using BuissnessLogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (clsClient.Deposit(1, 1055))
+            {
+                clsClient client = clsClient.findByID(1);
+                Console.WriteLine(client.AccountBalance);
+            }
+            else
+            {
+                Console.WriteLine("False");
+                clsClient client = clsClient.findByID(1);
+                Console.WriteLine(client.AccountBalance);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using BuissnessLogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsClient client = clsClient.findByID(1);
+            if (client == null) 
+            {
+                Console.WriteLine("not found");
+            }
+            else
+            {
+                Console.Write(client.PersonID);
+            }
+            Console.ReadKey();
+        }
+    }
+}
